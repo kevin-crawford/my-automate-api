1ea276fa7a0153fcfc419e0cf076e74cf4ccb5ae
Kevin Crawford <kcdouglas92@gmail.com>
2018-12-13 16:58:41 -0800

    added donenv requirement

--- auth/auth-strategy.js
+++ auth/auth-strategy.js
@@ -1,5 +1,5 @@
 'use strict';
-
+require('dotenv').config();
 const { Strategy: LocalStrategy } = require('passport-local');
 
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
